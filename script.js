7c5d34cabdb8910bc50c1480ad03a56888646b74
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2025-03-24 08:22:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const aumentaFonteBotao = document.getElementById('aumentar-fonte')
-let tamnahoFonteAtual = 1;
+document.addEventListener('DOMContentLoaded', function(){
+
+const botaoaAcessibilidade =document.getElementById ('botaoaAcessibilidade');
+const opcaoAcessibilidade = document.getElementById ('apresenta-lista')
+
+botaoaAcessibilidade.addEventListener('click',
+function(){
+    botaoaAcessibilidade.classList.toggle
+    ('rotacao-menu')
+    opcaoAcessibilidade.classList.toggle
+    ('apresentar-lista')
+})
+
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuirFonteBotao = document.getElementById('diminuir-fonte')
+
+let tamanhoFonteAtual = 1;
 
 aumentaFonteBotao.addEventListener('click', function() {
-tamanahoFonteAtual +=0.1;
-document.body.style.fontSize = ${tamnahoFonteAtual}rem
+    tamanhoFonteAtual += 0.1;
+    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
 })
+
+diminuirFonteBotao.addEventListener('click', function() {
+    tamanhoFonteAtual -= 0.1;
+    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
+})
+});
